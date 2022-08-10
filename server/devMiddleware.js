8ba1ae4df6474db17dc52f240826d620f1535a53
kevnflyn <rwestanton@gmail.com>
2022-08-10 16:21:34 +0200

    build: using resolve syntax for the same result in prod startup

--- server/devMiddleware.js
+++ server/devMiddleware.js
@@ -1,9 +1,5 @@
-const fs = require('fs')
 const path = require('path')
-const express = require('express')
 
-const chalk = require('chalk')
-const debug = require('debug')
 const webpack = require('webpack')
 const webpackDevMiddleware = require('webpack-dev-middleware')
 const WebpackHotMiddleware = require('webpack-hot-middleware')
@@ -11,16 +7,11 @@ const WebpackHotMiddleware = require('webpack-hot-middleware')
 const config = require('../build/webpack.config.dev')
 const commonMiddlware = require('./commonMiddleware.js')
 
-const debugDevMiddleware = debug('devMiddleware')
-
 const PORT = process.env.PORT || 3000
 
 module.exports = app => {
-  debugDevMiddleware(chalk.green('Compiling bundle...'))
-
   const compiler = webpack(config)
 
-  debugDevMiddleware(chalk.green('Initialising webpackDevMiddleware...'))
   const middleware = webpackDevMiddleware(compiler, {
     publicPath: config.output.publicPath
   })
@@ -34,8 +25,8 @@ module.exports = app => {
   commonMiddlware(app)
 
   app.get('*', (req, res) => {
-    res.sendFile(path.join(compiler.outputPath, 'index.html'));
-  });
+    res.sendFile(path.join(compiler.outputPath, 'index.html'))
+  })
 
   // app.get('/home|/login|/register|/password-reset-request|/password-reset|/app|/app/*', (req, res) => {
   //   const indexFile = path.resolve(compiler.outputPath, 'index.html')
--- server/prodMiddleware.js
+++ server/prodMiddleware.js
@@ -8,24 +8,25 @@ const commonMiddlware = require('./commonMiddleware.js')
 const PORT = process.env.PORT || 3000
 
 module.exports = app => {
-  app.use(express.static('dist'))
+  app.use(express.static(path.resolve('.', 'dist')))
 
   commonMiddlware(app)
 
   app.get('*', (req, res) => {
     console.log(`
-      sendFile to ${res.sendFile(path.resolve('dist', 'index.html'))}
+      sendFile to ${path.resolve('.', 'dist', 'index.html')}
     `)
 
-    res.sendFile(path.resolve('dist', 'index.html'))
-  });
+    app.use(express.static('.')) 
+    res.sendFile(path.resolve('.', 'dist', 'index.html'))
+  })
 
   app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`)
 
     setInterval(() => {
-      const used = process.memoryUsage().heapUsed / 1024 / 1024;
-      console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`);
+      const used = process.memoryUsage().heapUsed / 1024 / 1024
+      console.log(`The script uses approximately ${Math.round(used * 100) / 100} MB`)
     }, 1000)
   })
 }
