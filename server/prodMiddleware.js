41dacabdc726117ae80c836dc01e2ac690306f14
kevnflyn <rwestanton@gmail.com>
2022-08-10 15:10:50 +0200

    build: prod build cleanup

--- server/prodMiddleware.js
+++ server/prodMiddleware.js
@@ -1,19 +1,13 @@
 const fs = require('fs')
-// const http = require('http')
-// const https = require('https')
 const path = require('path')
 
 const chalk = require('chalk')
 const debug = require('debug')
 const express = require('express')
-// const fs = require('fs')
 
 const commonMiddlware = require('./commonMiddleware.js')
 
-const debugProd = debug('prodMiddlware')
-
-const port = 3000
-// const httpsPort = 4000
+const port = process.env.PORT || 3000
 
 module.exports = app => {
   app.use(express.static('dist'))
@@ -24,14 +18,5 @@ module.exports = app => {
     res.sendFile(path.resolve('dist', 'index.html'))
   })
 
-  // const sslOptions = {
-  //   key: fs.readFileSync('138.68.64.50+3-key.pem'),
-  //   cert: fs.readFileSync('138.68.64.50+3.pem')
-  // }
-
   app.listen(port)
-
-  // https.createServer(sslOptions, app).listen(httpsPort, () => {
-  //   debugProd(chalk.green(`prod server is running on port ${chalk.green(httpsPort)}`))
-  // })
 }
