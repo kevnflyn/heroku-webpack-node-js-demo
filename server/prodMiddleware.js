8368e1ad0668c9a5b2430eb779ffbf54cf1d62ae
kevnflyn <rwestanton@gmail.com>
2022-08-11 11:31:11 +0200

    build: use commonMiddleware

--- server/prodMiddleware.js
+++ server/prodMiddleware.js
@@ -3,31 +3,20 @@ const path = require('path')
 
 const express = require('express')
 
-// const commonMiddlware = require('./commonMiddleware.js')
+const commonMiddlware = require('./commonMiddleware.js')
 
 const PORT = process.env.PORT || 3000
 
 module.exports = app => {
-  app.use(express.static(path.resolve(__dirname,  '..', 'assets',))); 
-
   // the __dirname is the current directory from where the script is // running
   app.use(express.static(path.resolve(__dirname,  '..', 'dist',))); 
 
-  // commonMiddlware(app)
+  commonMiddlware(app)
 
   // send the user to index html page inspite of the url
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', 'dist', 'index.html'));
   }); 
-
-  // app.get('*', (req, res) => {
-  //   console.log(`
-  //     sendFile to ${path.resolve('.', 'dist', 'index.html')}
-  //   `)
-
-  //   app.use(express.static('.')) 
-  //   res.sendFile(path.resolve('.', 'dist', 'index.html'))
-  // })
 
   app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`)
